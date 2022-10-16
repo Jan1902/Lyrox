2793d538e1795ab886e7c01bed36521091f08a70
Jan-Hendrik Heinbokel <jan-hendrik.heinbokel@adesso.de>
2022-10-16 19:23:36 +0200

    Resolve Merge Conflict

--- src/Lyrox.Framework/LyroxClientFactory.cs
+++ src/Lyrox.Framework/LyroxClientFactory.cs
@@ -20,7 +20,11 @@ namespace Lyrox.Framework
     {
         private const string LogFilePath = "Log.txt";
 
+<<<<<<< HEAD
         public static ILyroxClient GetLyroxClient(LyroxConfiguration configuration, Action<IModuleManager>? pluginBuilding = null)
+=======
+        public static ILyroxClient GetLyroxClient(LyroxConfiguration configuration)
+>>>>>>> db4aa368f623507e9e82bee13b108847e82d382d
         {
             var builder = new ContainerBuilder();
             var loggerConfiguration = new LoggerConfiguration()
