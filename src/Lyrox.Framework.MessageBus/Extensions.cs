c9f0bf715c1dda3040e149a71b25fbb89772dd0f
Jan-Hendrik Heinbokel <jan-hendrik.heinbokel@adesso.de>
2022-12-24 12:49:33 +0100

    Register as Publish And Subscribe Bus seperately

--- src/Lyrox.Framework.MessageBus/Extensions.cs
+++ src/Lyrox.Framework.MessageBus/Extensions.cs
@@ -11,20 +11,30 @@ namespace Lyrox.Framework.Messaging
     public static class Extensions
     {
         public static void RegisterMessageBus(this ContainerBuilder builder)
-            => builder.RegisterType<MessageBus>().As<IMessageBus>().InstancePerLifetimeScope();
+            => builder.RegisterType<MessageBus>()
+                .As<IMessageBus>()
+                .As<IPublishBus>()
+                .As<ISubscribeBus>()
+                .InstancePerLifetimeScope();
 
         public static void RegisterAutofacMessagebus(this ContainerBuilder builder)
-            => builder.RegisterType<AutoFacMessageBus>().As<IMessageBus>().InstancePerLifetimeScope();
+            => builder.RegisterType<AutoFacMessageBus>()
+                .As<IMessageBus>()
+                .InstancePerLifetimeScope();
 
         public static void RegisterMessageHandler<TMessage, THandler>(this ContainerBuilder builder)
             where TMessage : IMessage
             where THandler : IMessageHandler<TMessage>
-            => builder.RegisterType<THandler>().As<IMessageHandler<TMessage>>().InstancePerDependency();
+            => builder.RegisterType<THandler>()
+                .As<IMessageHandler<TMessage>>()
+                .InstancePerDependency();
 
         public static void RegisterRequestHandler<TRequest, TResponse, THandler>(this ContainerBuilder builder)
             where TRequest : IRequest<TResponse>
             where THandler : IRequestHandler<TRequest, TResponse>
             where TResponse : IResponse
-            => builder.RegisterType<THandler>().As<IRequestHandler<TRequest, TResponse>>().InstancePerDependency();
+            => builder.RegisterType<THandler>()
+                .As<IRequestHandler<TRequest, TResponse>>()
+                .InstancePerDependency();
     }
 }
