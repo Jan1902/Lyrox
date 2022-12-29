89bff1699e385a87b4257fb8cdc1ac14566b2d03
Jan-Hendrik Heinbokel <jan-hendrik.heinbokel@adesso.de>
2022-12-29 12:48:31 +0100

    Sketched out the new Packet Handler Interface

--- src/Lyrox.Framework.Core/Networking/Abstraction/Packet/Handler/IPacketHandler.cs
+++ src/Lyrox.Framework.Core/Networking/Abstraction/Packet/Handler/IPacketHandler.cs
@@ -1,7 +1,28 @@
-﻿namespace Lyrox.Framework.Core.Networking.Abstraction.Packet.Handler
+﻿using Lyrox.Framework.Messaging.Abstraction.Handlers;
+using Lyrox.Framework.Messaging.Abstraction.Messages;
+
+namespace Lyrox.Framework.Core.Networking.Abstraction.Packet.Handler
 {
     public interface IPacketHandler<T> where T : IClientBoundNetworkPacket
     {
         Task HandlePacket(T networkPacket);
+    }
+
+    public interface IPacketMessageHandler<TPacket> : IMessageHandler<PacketReceivedMessage<TPacket>>
+        where TPacket : IClientBoundNetworkPacket
+    {
+        Task IMessageHandler<PacketReceivedMessage<TPacket>>.HandleMessageAsync(PacketReceivedMessage<TPacket> message)
+            => HandlePacketAsync(message.Packet);
+
+        Task HandlePacketAsync(TPacket packet);
+    }
+
+    public record PacketReceivedMessage<TPacket> : IMessage
+        where TPacket : IClientBoundNetworkPacket
+    {
+        public TPacket Packet { get; }
+
+        public PacketReceivedMessage(TPacket packet)
+            => Packet = packet;
     }
 }
